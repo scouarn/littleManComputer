e0fded57fa47926b830b104a28fea6767444f2b5
scouarn <admin@scouarn.fr>
2022-10-03 22:32:02 +0200

    Architecture générale de la page web

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
-console.log("OK!");
+const memory = new Array(100);
 
+function init_mem() {
+    const mem_el = document.getElementById("memory");
 
+    for (i = 0; i < 100; i++) {
+        const e = document.createElement("input");
+        e.setAttribute("type", "number");
+        e.setAttribute("addr", i);
+        e.classList.add("cell");
+        e.value = 0; 
+
+        mem_el.appendChild(e);
+        memory[i] = e; 
+    }
+    
+    memory[0].classList.add("current");
+}
 
