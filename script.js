ba6ebc1321287965b1226a2f387844c07c7df722
scouarn <admin@scouarn.fr>
2022-10-04 16:38:43 +0200

    Logique du simulateur

--- script.js
+++ script.js
@@ -1,24 +1,78 @@
 
-
-const SIZE = 100;
 const memory = new Array();
 
 let ip = 0;
 let acc = 0; 
 
+function next() {
+    ip = (ip+1) % 100;
+}
+
+function mem_get(a) {
+    return parseInt(memory[a].value);
+}
+
+function mem_set(a, v) {
+    memory[a].value = v;
+}
 
 function step() {
     
     // fetch    
     //println("Étape " + ip);
-    const op = memory[ip].value;
-    ip = (ip+1) % SIZE;
+    const op = mem_get(ip);
     
     // execute 
+    const ins_1_arg = [
+        { asm: "ADD", f: (a) => { acc += mem_get(a);      } },
+        { asm: "SUB", f: (a) => { acc -= mem_get(a);      } },
+        { asm: "STA", f: (a) => { mem_set(a, acc);        } },
+        { asm: "LDA", f: (a) => { acc = mem_get(a);       } },
+        { asm: "BRA", f: (a) => { ip = a;                 } },
+        { asm: "BRZ", f: (a) => { if (acc == 0) ip = a;   } },
+        { asm: "BRP", f: (a) => { if (acc >= 0) ip = a;   } },
+    ];
+    
+    let asm; 
+   
+    if (op == 0) {
+        alert("Fin du programme"); 
+        asm = "HLT";
+    }
+    else if (op == 901) {
+        asm = "INP";
+        
+        const inp = parseInt(prompt("Entrée : ", "0"));
 
+        if (isFinite(inp))
+            acc = inp;
+        else 
+            acc = 0;
+
+        next();
+    }
+    else if (op == 902) {
+        alert("Sortie : " + acc);
+        asm = "OUT"; 
+        next();
+    }
+    else if (op >= 100 && op <= 899) { 
+        const idx = Math.floor(op/100) - 1;
+        const arg = op % 100;
+        ins_1_arg[idx].f(arg);
+        asm = ins_1_arg[idx].asm;
+        next();
+    }
+    else {
+        alert("Instruction invalide : " + op); 
+        asm = "???";
+        console.log(op);
+    }
     
     // display
+    console.log(asm);
     update_regs();
+
 }
 
 
@@ -51,7 +105,8 @@ function println(x) {
 function init() {
 
     const grid = document.getElementById("memory");
-    for (let i = 0; i < SIZE; i++) {
+
+    for (let i = 0; i < 100; i++) {
         const container = document.createElement("div");
         container.classList.add("cell");
     
